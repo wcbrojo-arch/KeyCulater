2c44dbff2f98e4a03fc438fbed5647f79459c0f4
wcbrojo-arch <wcbrojo@gmail.com>
2025-10-03 19:45:22 -0500

    here

---
+++ index.js
@@ -0,0 +1,14 @@
+// calculater program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value += input;
+}
+
+function clearDisplay(){
+  display.value = "";
+}
+function calculate(){
+    display.value = eval(display.value);
+}
